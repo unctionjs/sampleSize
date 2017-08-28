49e443a5a59676ee14a3214e2cc0a5426aa024c1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:51 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import {test} from "tap"
 import {range} from "ramda"
 import {intersection} from "ramda"
 
-import sampleSize from "./"
+import sampleSize from "./index"
 
 test(({includes, end}) => {
   includes(intersection(sampleSize(1, range(1, 100)), range(1, 100)), [])
