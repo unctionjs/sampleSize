97f4ca34d6593093bd637d29530003b4954a05be
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-10-08 15:02:47 -0700

    Using the new shuffle

--- test.js
+++ test.js
@@ -1,37 +1,38 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
-
+import {equals} from "ramda"
+import {or} from "ramda"
 
 import sampleSize from "./index"
 
-test(({includes, end}) => {
+test(({ok, end}) => {
   const value = sampleSize(2)(["a", "b"])
 
-  includes(
-    value,
-    ["a"]
-  )
-
-  includes(
-    value,
-    ["b"]
-  )
+  ok(or(equals(["a", "b"], ["b", "a"]))(value))
 
   end()
 })
 
-test(({includes, end}) => {
+test(({ok, end}) => {
+  const value = sampleSize(1)(["a", "b"])
+
+  ok(or(equals(["a"], ["b"]))(value))
+
+  end()
+})
+
+test(({ok, end}) => {
   const value = sampleSize(2)("ab")
 
-  includes(
-    value,
-    "a"
-  )
+  ok(or(equals("ab", "ba"))(value))
 
-  includes(
-    value,
-    "b"
-  )
+  end()
+})
+
+test(({ok, end}) => {
+  const value = sampleSize(1)("ab")
+
+  ok(or(equals("a", "b"))(value))
 
   end()
 })
