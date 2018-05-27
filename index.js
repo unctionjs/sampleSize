af87b64004a3412db65419830647170ed8cfcda8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-import shuffle from "@unction/shuffle"
-import takeFirst from "@unction/takefirst"
-import type {OrderedFunctorType} from "types"
-import type {NumberType} from "types"
-import type {UnaryFunctionType} from "types"
-
-export default function sample (size: NumberType): UnaryFunctionType {
-  return function sampleSize (list: OrderedFunctorType): OrderedFunctorType {
-    return takeFirst(size)(shuffle(list))
-  }
+import shuffle from "@unction/shuffle";
+import takeFirst from "@unction/takefirst";
+export default function sample(size) {
+  return function sampleSize(list) {
+    return takeFirst(size)(shuffle(list));
+  };
 }
